1bc5a49ab37f290ce0fa10d93b1532015dd1d594
Marcus <6180175+MKindberg@users.noreply.github.com>
2026-06-17 22:43:25 +0200

    Add support for a config file

--- src/main.zig
+++ src/main.zig
@@ -9,12 +9,57 @@ const Command = enum {
     Help,
 };
 
+const Config = struct {
+    const Self = @This();
+
+    const Error = error{
+        InvalidConfig,
+    };
+
+    fn init(gpa: std.mem.Allocator, io: std.Io, environ: std.process.Environ) !Self {
+        const config_path = config_path: {
+            if (environ.getPosix("XDG_CONFIG_HOME")) |config_home| {
+                break :config_path std.fmt.allocPrint(gpa, "{s}/retrace/config", .{config_home}) catch unreachable;
+            } else if (environ.getPosix("HOME")) |home| {
+                break :config_path std.fmt.allocPrint(gpa, "{s}/.config/retrace/config", .{home}) catch unreachable;
+            } else {
+                return Self{};
+            }
+        };
+        defer gpa.free(config_path);
+
+        const config_data = std.Io.Dir.cwd().readFileAlloc(io, config_path, gpa, .unlimited) catch |e| {
+            switch (e) {
+                error.FileNotFound => return Self{},
+                else => return e,
+            }
+        };
+        defer gpa.free(config_data);
+        var it = std.mem.splitScalar(u8, config_data, '\n');
+        while (it.next()) |line| {
+            const l = std.mem.trimStart(u8, line, &std.ascii.whitespace);
+            if (std.mem.startsWith(u8, l, "//")) continue;
+            if (l.len == 0) continue;
+            const eq_idx = std.mem.findScalar(u8, l, '=') orelse return Error.InvalidConfig;
+
+            const key = std.mem.trim(u8, l[0..eq_idx], " ");
+            const value = std.mem.trim(u8, l[eq_idx + 1 ..], " ");
+
+            _ = key;
+            _ = value;
+        }
+        return Self{};
+    }
+};
+
 fn printHelp() void {}
 
 pub fn main(init: std.process.Init) !u8 {
     var args = init.minimal.args.iterate();
     _ = args.skip();
 
+    const config = try Config.init(init.gpa, init.io, init.minimal.environ);
+    _ = config;
 
     const command_str = args.next() orelse {
         printHelp();
@@ -42,9 +87,8 @@ pub fn main(init: std.process.Init) !u8 {
             try db.insert(init.gpa, dir, entry);
         },
         .List => {
-             const res = try db.fetch(init.arena.allocator(), dir);
-             for (res.items) |r| std.debug.print("{s}\n", .{r});
-
+            const res = try db.fetch(init.arena.allocator(), dir);
+            for (res.items) |r| std.debug.print("{s}\n", .{r});
         },
         .Help => {
             printHelp();
